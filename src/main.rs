41673683337021f447a6a7dcc974f38daa63c042
SimY4 <alex.simkin@outlook.com>
2022-12-02 14:49:56 +1100

    y2022 day1

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod y2015;
 mod y2019;
 mod y2020;
 mod y2021;
+mod y2022;
 
 fn main() {
     y2015::run();
     y2019::run();
     y2020::run();
     y2021::run();
+    y2022::run();
 }
---
+++ src/y2022/mod.rs
@@ -0,0 +1,8 @@
+pub mod day1;
+
+pub fn run() {
+    println!("y2022:");
+    println!("  day1:");
+    println!("    ├ solution #1: {:?}", day1::solve(day1::INPUT));
+    println!("    └ solution #2: {}", day1::solve2(day1::INPUT));
+}

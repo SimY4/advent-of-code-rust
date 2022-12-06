dfbd183a89fdb17e74f870930e883fa77607ff5f
SimY4 <alex.simkin@outlook.com>
2022-12-06 19:37:19 +1100

    y2022 day6

--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -1,8 +1,12 @@
 pub mod day1;
+pub mod day6;
 
 pub fn run() {
     println!("y2022:");
     println!("  day1:");
     println!("    ├ solution #1: {:?}", day1::solve(day1::INPUT));
     println!("    └ solution #2: {}", day1::solve2(day1::INPUT));
+    println!("  day6:");
+    println!("    ├ solution #1: {:?}", day6::solve(day6::INPUT));
+    println!("    └ solution #2: {:?}", day6::solve2(day6::INPUT));
 }
